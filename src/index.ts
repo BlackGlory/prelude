de57f1c12a7852007f4f54ff45f7b1d8a92f655d
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-03-05 18:21:45 +0000

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+export { assert } from '@blackglory/errors'
+export { go } from '@blackglory/go'
+export { log } from '@blackglory/log'
+export { lazy } from 'extra-lazy'
+export { pass } from '@blackglory/pass'
+export { toArray, toArrayAsync } from 'iterable-operator'
+export {
+  Arrayable
+, Awaitable
+, Constructor
+, Falsy
+, Getter
+, Json
+, NonEmptyArray
+, Nullable
+, Nullish
+} from 'justypes'
+export {
+  isArray
+, isAsyncIterable
+, isBigInt
+, isBoolean
+, isChar
+, isDate
+, isEmptyArray
+, isEmptyObject
+, isError
+, isFalsy
+, isFunction
+, isIterable
+, isJson
+, isNull
+, isNumber
+, isObject
+, isPlainObject
+, isPromise
+, isPromiseLike
+, isString
+, isUndefined
+, isntArray
+, isntAsyncIterable
+, isntBigInt
+, isntBoolean
+, isntChar
+, isntDate
+, isntEmptyArray
+, isntEmptyObject
+, isntError
+, isntFalsy
+, isntFunction
+, isntIterable
+, isntJson
+, isntNull
+, isntNumber
+, isntObject
+, isntPlainObject
+, isntPromise
+, isntPromiseLike
+, isntString
+, isntUndefined
+} from '@blackglory/types'
